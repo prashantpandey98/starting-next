92e34681e311ae87c4e405d8e59983a0d40c2245
Prashant Pandey <yofalcon98@gmail.com>
2023-08-29 21:43:04 +0530

    Names hyperlinked in aboutus

--- pages/aboutus/developer.js
+++ pages/aboutus/developer.js
@@ -1,4 +1,8 @@
 function Developer() {
-  return <h1>Prashant Pandey</h1>;
+  return (
+    <>
+      <h1>Prashant Pandey</h1>;
+    </>
+  );
 }
 export default Developer;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,4 +1,18 @@
+import Link from "next/link";
+
 function NewsPage() {
-  return <h1>This is the news page</h1>;
+  return (
+    <>
+      <h1>This is the news page</h1>;
+      <ul>
+        <li>
+          <Link href="/news/next-js-is-a-great-framework">
+            NEXT JS is a Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </>
+  );
 }
 export default NewsPage;
